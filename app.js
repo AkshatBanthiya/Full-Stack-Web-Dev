2b6f8ee072d07054b75c747d446264835c43697e
Akshat Banthiya <akshatbanthiya@Akshats-MacBook-Air.local>
2021-09-29 16:07:20 +0530

    primitive data type

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("hello world")
+console.warn("hello warning")
+console.error("hello to error")
+
+//string
+console.log(typeof "apple")
+console.log(typeof "a")
+//number
+console.log(typeof 1)
+console.log(typeof 1.4)
+console.log(typeof -1.4)
+console.log(typeof -1)
+console.log(typeof 1234567890)
+console.log(typeof -1234567890)
+//bool
+console.log(typeof true)
+console.log(typeof false)
