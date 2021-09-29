4b71f89c4a282037d5d158b09e51e632ea216eeb
Akshat Banthiya <akshatbanthiya@Akshats-MacBook-Air.local>
2021-09-29 17:49:38 +0530

    operators in Java Script

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
-console.log("hello world")
-console.warn("hello warning")
-console.error("hello to error")
+// Operators
 
-//string
-console.log(typeof "apple")
-console.log(typeof "a")
-//number
-console.log(typeof 1)
-console.log(typeof 1.4)
-console.log(typeof -1.4)
-console.log(typeof -1)
-console.log(typeof 1234567890)
-console.log(typeof -1234567890)
-//bool
-console.log(typeof true)
-console.log(typeof false)
+
+var a = 2
+var b = 10
+
+// Arithmetic operators
+
+console.log('a + b =', a + b)
+console.log('b - a =', b - a)
+console.log('b / a =', b / a)
+console.log('b * a =', b * a)
+console.log('b ** a =', b ** a)
+console.log('b % a =', b % a)
+
+
+// Assignment Operators
+console.log('b ', b)
+b += 10
+console.log('b+=10 ', b)
+b++
+console.log('b++ ', b)
+b -= 10
+console.log('b-=10 ', b)
+b--
+console.log('b-- ', b)
+
+var c = 10
+
+console.warn(c++)
+console.log(c)
+// console.warn(++c)
+
+// Comparison operators
+
+var d = 10
+var e = 2
+var f = '10'
+
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d == e)
+
+console.warn(d == f)
+console.warn(d === f)
+
+// Logical operators
+
+console.log('true && true ', true && true)
+console.log('true && false', true && false)
+console.log('false && true', false && true)
+console.log('false && false', false && false)
+
+console.log('true || true ', true || true)
+console.log('true || false', true || false)
+console.log('false || true', false || true)
+console.log('false || false', false || false)
