233a78331f29d164dc2b25889a99f9f23d305580
Akshat Banthiya <akshatbanthiya@Akshats-MacBook-Air.local>
2021-09-30 22:00:13 +0530

    functions

--- Java_script/app.js
+++ Java_script/app.js
@@ -1,10 +1,33 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+
+ var list = document.querySelector('ul')
+ // function greet(name = 'John', age = 99) {
+ //     // console.log('Hello')
+ //     console.log(name, age)
+ //     // console.log('Hey ' + name + " Your age is " + age)/
+ // }
+
+ // greet()
+
+ var arr = ['Go the GYM ', 'cook oatmeal', 'Eat']
+
+ arr.push('Complete the assignment')
+
+ for (var i = 0; i < arr.length; i++) {
+     var element = document.createElement('li')
+     var textNode = document.createTextNode(arr[i])
+     element.appendChild(textNode)
+     list.appendChild(element)
+ // function add(a = 0, b = 0) {
+ //     var sum = a + b
+ //     return sum
+ // }
+
+ } 
+ var add = (a = 0, b = 0) => {
+     var sum = a + b
+     return sum
+ }
 
 
-const callbackfunc = (event) => {
-    body.classList.toggle('dark')
-}
 
-
-button.addEventListener('click', callbackfunc)
+ console.log(add(10, 20))
