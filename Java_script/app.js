679673111f0a765c967adc1f8582d8a7fcde083d
Akshat Banthiya <akshatbanthiya@Akshats-MacBook-Air.local>
2021-10-08 14:07:15 +0530

    promises in JS

--- Java_script/app.js
+++ Java_script/app.js
@@ -1,16 +1,9 @@
-const list = document.querySelector('ul')
-let arr =['one','two','three','four']
-console.log("list")
-
-arr.forEach(chore =>{
-  const element=document.createElement('li')
-  const textNode=document.createTextNode(chore)
-  element.appendChild(textNode)
-
-
-  element.addEventListener('click',(e)=>{
-    const useInput = Window.prompt("are you sure you want to delete "${chore}""`")
-  })
-
-  list.appendChild(element)
+let name = new Promise((resolve,reject) =>{
+  setTimeout(() => {
+    resolve('Akshat')
+  }, 3000)
 })
+console.log(name)
+setTimeout(() => {
+  console.log(name)
+}, 4000);
